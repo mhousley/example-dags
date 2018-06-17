6d24541de1e63a59ef900d41c43a8cc0ce856dae
Mac User <macuser@Macs-MacBook-Pro.local>
2018-06-16 21:18:22 -0400

    Update example_workflow; add .gitignore
    example_workflow.py now includes call to AWS Glue.

--- example_workflow.py
+++ example_workflow.py
@@ -18,11 +18,24 @@
 # under the License.
 
 import airflow
-from builtins import range
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import DAG
+from airflow.operators import BaseOperator
 from datetime import timedelta
+from airflow.contrib.hooks.aws_hook import AwsHook
+
+
+class StartCrawler(BaseOperator):
+    def __init__(
+            self,
+            *args,
+            **kwargs):
+        super(StartCrawler, self).__init__(*args, **kwargs)
+
+    def execute(self, context):
+        self.log.info('Triggering crawler.')
+        glue_hook = AwsHook()
+        client = glue_hook.get_client_type(client_type='glue')
+        client.start_crawler(Name='s3 crawler')
 
 
 args = {
@@ -35,26 +48,8 @@ dag = DAG(
     schedule_interval='0 0 * * *',
     dagrun_timeout=timedelta(minutes=60))
 
-cmd = 'ls -l'
-run_this_last = DummyOperator(task_id='run_this_last', dag=dag)
-
-run_this = BashOperator(
-    task_id='run_after_loop', bash_command='echo 1', dag=dag)
-run_this.set_downstream(run_this_last)
-
-for i in range(3):
-    i = str(i)
-    task = BashOperator(
-        task_id='runme_' + i,
-        bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        dag=dag)
-    task.set_downstream(run_this)
-
-task = BashOperator(
-    task_id='also_run_this',
-    bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    dag=dag)
-task.set_downstream(run_this_last)
+run_this = StartCrawler(
+    task_id='start_crawler', dag=dag)
 
 if __name__ == "__main__":
     dag.cli()
